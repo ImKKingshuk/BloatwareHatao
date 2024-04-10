005f6d137de0c6972e55c4c24a4ff61c5f05dbb7
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2024-04-11 04:01:42 +0530

    updated

--- Safe/xiaomi/hyperos-1.sh
+++ Safe/xiaomi/hyperos-1.sh
@@ -2,9 +2,16 @@
 
  
 declare -a bloatware=(
-    "com.samsung.bloatware1"
-    "com.samsung.bloatware2"
-    "com.samsung.bloatware3"
+    "com.miui.android.fashiongallery"
+    "com.miui.bugreport"
+    "com.miui.msa.global"
+    "com.xiaomi.glgm"
+    "com.xiaomi.joyose"
+    "com.xiaomi.mipicks"
+    "com.xiaomi.payment"
+    "cn.wps.xiaomi.abroad.lite"
+    "in.amazon.mShop.android.shopping"
+
     # Add more bloatware package names as needed
 )
 
