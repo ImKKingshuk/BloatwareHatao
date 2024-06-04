e0b593daae48855d00faf5ccc58a484f3aa466b5
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2024-06-04 11:26:49 +0530

    updated

--- Ultra/huawei/emui-12.sh
+++ Ultra/huawei/emui-12.sh
@@ -2,9 +2,17 @@
 
  
 declare -a bloatware=(
-    "com.samsung.bloatware1"
-    "com.samsung.bloatware2"
-    "com.samsung.bloatware3"
+  "com.google.android.apps.youtube.music"
+    "com.google.android.calendar"
+    "com.google.android.keep"
+    "com.google.android.youtube"
+    "com.google.android.apps.photos"
+    "com.google.android.apps.maps"
+    "com.google.android.apps.magazines"
+    "com.google.android.apps.docs"
+    "com.facebook.system"
+    "com.facebook.services"
+    "com.facebook.appmanager"
     # Add more bloatware package names as needed
 )
 
