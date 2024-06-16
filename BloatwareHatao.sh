a9129a725424c38c549bd5afc6df8d68cd7488b7
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2024-06-16 07:25:04 +0530

    updated

--- BloatwareHatao.sh
+++ BloatwareHatao.sh
@@ -229,7 +229,7 @@ show_os_version_menu() {
             case $os_choice in
                 1) os_version="oxygenos-14" ;;
                 2) os_version="oxygenos-13" ;;
-                3) os_version="oxygenos-11" ;;
+                3) os_version="oxygenos-12" ;;
                 4) show_manufacturer_menu "$cleaner_type"; return ;;
                 5) echo "Exiting..."; exit ;;
                 *) echo "Invalid choice. Please try again."; show_os_version_menu "$manufacturer" "$cleaner_type"; return ;;
