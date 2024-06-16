2fd452988e7b7cf730c2ebe6b6f68fd922bb1a87
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2024-06-16 06:56:46 +0530

    updated

--- BloatwareHatao.sh
+++ BloatwareHatao.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-
 print_banner() {
     local banner=(
         "******************************************"
         "*              BloatwareHatao            *"
         "*     Android Bloatware Removal Tool     *"
-        "*                  v1.2.1                *"
+        "*                  v1.3.1                *"
         "*      ----------------------------      *"
         "*                        by @ImKKingshuk *"
         "* Github- https://github.com/ImKKingshuk *"
@@ -20,27 +19,33 @@ print_banner() {
 }
 
 remove_bloatware() {
-    manufacturer=$1
-    os_version=$2
-    cleaner_type=$3
-    script_path="https://raw.githubusercontent.com/ImKKingshuk/BloatwareHatao/main/$cleaner_type/$manufacturer/$os_version.sh"
+    local manufacturer=$1
+    local os_version=$2
+    local cleaner_type=$3
+    local script_path="https://raw.githubusercontent.com/ImKKingshuk/BloatwareHatao/main/$cleaner_type/$manufacturer/$os_version.sh"
 
-    echo "Fetching bloatware removal script from $manufacturer for $os_version ($cleaner_type)..."
+    echo "Fetching bloatware removal script for $manufacturer $os_version ($cleaner_type)..."
     curl -sSL "$script_path" | bash
 
-    echo "Bloatware removal completed."
+    if [ $? -eq 0 ]; then
+        echo "Bloatware removal completed successfully."
+    else
+        echo "Error occurred during bloatware removal."
+    fi
 }
 
-
 remove_bloatware_manual() {
-    package_name=$1
+    local package_name=$1
 
     echo "Attempting to remove bloatware with package name: $package_name"
     adb shell pm uninstall --user 0 "$package_name"
 
-    echo "Bloatware removal completed."
+    if [ $? -eq 0 ]; then
+        echo "Bloatware removal completed successfully."
+    else
+        echo "Error occurred during bloatware removal."
+    fi
 }
-
 
 show_cleaner_type_menu() {
     echo "Select the cleaner type:"
@@ -53,36 +58,16 @@ show_cleaner_type_menu() {
     read -p "Enter your choice: " cleaner_type_choice
 
     case $cleaner_type_choice in
-        1)
-            cleaner_type="Safe"
-            ;;
-        2)
-            cleaner_type="Pro"
-            ;;
-        3)
-            cleaner_type="Ultra"
-            ;;
-        4)
-            cleaner_type="Manual"
-            ;;
-        5)
-            echo "Exiting..."
-            exit
-            ;;
-        *)
-            echo "Invalid choice. Please try again."
-            show_cleaner_type_menu
-            return
-            ;;
+        1) cleaner_type="Safe" ;;
+        2) cleaner_type="Pro" ;;
+        3) cleaner_type="Ultra" ;;
+        4) manual_cleaner_menu ;;
+        5) echo "Exiting..."; exit ;;
+        *) echo "Invalid choice. Please try again."; show_cleaner_type_menu ;;
     esac
 
-    if [ "$cleaner_type" = "cleaner4" ]; then
-        manual_cleaner_menu
-    else
-        show_manufacturer_menu "$cleaner_type"
-    fi
+    show_manufacturer_menu "$cleaner_type"
 }
-
 
 manual_cleaner_menu() {
     echo "Manual Bloatware Cleaner"
@@ -95,21 +80,14 @@ manual_cleaner_menu() {
     read -p "Are you sure you want to remove this bloatware? (Yes/No): " confirmation
 
     case $confirmation in
-        [Yy][Ee][Ss])
-            remove_bloatware_manual "$package_name"
-            ;;
-        [Nn][Oo])
-            echo "Bloatware removal cancelled."
-            ;;
-        *)
-            echo "Invalid choice. Bloatware removal cancelled."
-            ;;
+        [Yy][Ee][Ss]) remove_bloatware_manual "$package_name" ;;
+        [Nn][Oo]) echo "Bloatware removal cancelled." ;;
+        *) echo "Invalid choice. Bloatware removal cancelled." ;;
     esac
 }
 
-
 show_manufacturer_menu() {
-    cleaner_type=$1
+    local cleaner_type=$1
 
     echo "Select your device manufacturer:"
     echo "1. Samsung"
@@ -128,116 +106,85 @@ show_manufacturer_menu() {
     read -p "Enter your choice: " manufacturer_choice
 
     case $manufacturer_choice in
-        1)
-            manufacturer="samsung"
-            ;;
-        2)
-            manufacturer="xiaomi"
-            ;;
-        3)
-            manufacturer="huawei"
-            ;;
-        4)
-            manufacturer="oneplus"
-            ;;
-        5)
-            manufacturer="vivo"
-            ;;
-        6)
-            manufacturer="oppo"
-            ;;
-        7)
-            manufacturer="realme"
-            ;;
-        8)
-            manufacturer="nothing"
-            ;;
-        9)
-            manufacturer="honor"
-            ;;
-        10)
-            manufacturer="motorola"
-            ;;
-        11)
-            manufacturer="meizu"
-            ;;
-        12)
-            echo "Exiting..."
-            exit
-            ;;
-        *)
-            echo "Invalid choice. Please try again."
-            show_manufacturer_menu "$cleaner_type"
-            return
-            ;;
+        1) manufacturer="samsung" ;;
+        2) manufacturer="xiaomi" ;;
+        3) manufacturer="huawei" ;;
+        4) manufacturer="oneplus" ;;
+        5) manufacturer="vivo" ;;
+        6) manufacturer="oppo" ;;
+        7) manufacturer="realme" ;;
+        8) manufacturer="nothing" ;;
+        9) manufacturer="honor" ;;
+        10) manufacturer="motorola" ;;
+        11) manufacturer="meizu" ;;
+        12) echo "Exiting..."; exit ;;
+        *) echo "Invalid choice. Please try again."; show_manufacturer_menu "$cleaner_type" ;;
     esac
 
     show_os_version_menu "$manufacturer" "$cleaner_type"
 }
 
-
 show_os_version_menu() {
-    manufacturer=$1
-    cleaner_type=$2
+    local manufacturer=$1
+    local cleaner_type=$2
 
     echo "Select your $manufacturer's OS version:"
 
-
     case $manufacturer in
         "samsung")
-            echo "1. Samsung OneUI 6.0"
-            echo "2. Samsung OneUI 5.0"
-            echo "3. Samsung OneUI 4.0"
+            echo "1. OneUI 6"
+            echo "2. OneUI 5"
+            echo "3. OneUI 4"
             ;;
         "xiaomi")
-            echo "1. Xiaomi HyperOS 1"
-            echo "2. Xiaomi MIUI 14"
-            echo "3. Xiaomi MIUI 13"
+            echo "1. HyperOS 1"
+            echo "2. MIUI 14"
+            echo "3. MIUI 13"
             ;;
         "huawei")
-            echo "1. Huawei EMUI 14"
-            echo "2. Huawei EMUI 13"
-            echo "3. Huawei EMUI 12"
+            echo "1. EMUI 14"
+            echo "2. EMUI 13"
+            echo "3. EMUI 12"
             ;;
         "oneplus")
-            echo "1. OnePlus OxygenOS 14"
-            echo "2. OnePlus OxygenOS 13"
-            echo "3. OnePlus OxygenOS 12"
+            echo "1. OxygenOS 14"
+            echo "2. OxygenOS 13"
+            echo "3. OxygenOS 12"
             ;;
         "vivo")
-            echo "1. Vivo FuntouchOS 14"
-            echo "2. Vivo FuntouchOS 13"
-            echo "3. Vivo FuntouchOS 12"
+            echo "1. FuntouchOS 11"
+            echo "2. FuntouchOS 10"
+            echo "3. FuntouchOS 9"
             ;;
         "oppo")
-            echo "1. OPPO ColorOS 14"
-            echo "2. OPPO ColorOS 13"
-            echo "3. OPPO ColorOS 12"
+            echo "1. ColorOS 6"
+            echo "2. ColorOS 7"
+            echo "3. ColorOS 8"
             ;;
         "realme")
-            echo "1. Realme RealmeUI 5"
-            echo "2. Realme RealmeUI 4"
-            echo "3. Realme RealmeUI 3"
+            echo "1. RealmeUI 1"
+            echo "2. RealmeUI 2"
+            echo "3. RealmeUI 3"
             ;;
         "nothing")
-            echo "1. NothingOS 3"
-            echo "2. NothingOS 2.5"
-            echo "3. NothingOS 2"
+            echo "1. NothingOS 1"
+            echo "2. NothingOS 2"
+            echo "3. NothingOS 3"
             ;;
         "honor")
-            echo "1. Honor MagicOS 8"
-            echo "2. Honor MagicOS 7"
-            echo "3. Honor MagicOS 6"
+            echo "1. MagicUI 2"
+            echo "2. MagicUI 3"
+            echo "3. MagicUI 4"
             ;;
         "motorola")
-            echo "1. Motorola HelloUI 1"
-            echo "2. Motorola MyUX 14"
-            echo "3. Motorola MyUX 13"
+            echo "1. MyUX 1"
+            echo "2. MyUX 2"
+            echo "3. MyUX 3"
             ;;
         "meizu")
-            echo "1. Meizu FlymeAIOS 11"
-            echo "2. Meizu FlymeOS 10"
-            echo "3. Meizu FlymeOS 9"
+            echo "1. FlymeOS 9"
+            echo "2. FlymeOS 10"
+            echo "3. FlymeOS 11"
             ;;
     esac
 
@@ -246,36 +193,122 @@ show_os_version_menu() {
     echo "--------------------------------------"
     read -p "Enter your choice: " os_choice
 
-    case $os_choice in
-        1)
-            os_version="${manufacturer}/oneui-1.sh"
+    local os_version=""
+    case $manufacturer in
+        "samsung")
+            case $os_choice in
+                1) os_version="oneui-1" ;;
+                2) os_version="oneui-2" ;;
+                3) os_version="oneui-3" ;;
+                4) show_manufacturer_menu "$cleaner_type"; return ;;
+                5) echo "Exiting..."; exit ;;
+                *) echo "Invalid choice. Please try again."; show_os_version_menu "$manufacturer" "$cleaner_type"; return ;;
+            esac
             ;;
-        2)
-            os_version="${manufacturer}/oneui-2.sh"
+        "xiaomi")
+            case $os_choice in
+                1) os_version="miui-10" ;;
+                2) os_version="miui-11" ;;
+                3) os_version="miui-14" ;;
+                4) show_manufacturer_menu "$cleaner_type"; return ;;
+                5) echo "Exiting..."; exit ;;
+                *) echo "Invalid choice. Please try again."; show_os_version_menu "$manufacturer" "$cleaner_type"; return ;;
+            esac
             ;;
-        3)
-           os_version="${manufacturer}/oneui-3.sh"
+        "huawei")
+            case $os_choice in
+                1) os_version="emui-12" ;;
+                2) os_version="emui-13" ;;
+                3) os_version="emui-14" ;;
+                4) show_manufacturer_menu "$cleaner_type"; return ;;
+                5) echo "Exiting..."; exit ;;
+                *) echo "Invalid choice. Please try again."; show_os_version_menu "$manufacturer" "$cleaner_type"; return ;;
+            esac
             ;;
-        4)
-            show_manufacturer_menu "$cleaner_type"
-            return
+        "oneplus")
+            case $os_choice in
+                1) os_version="oxygenos-9" ;;
+                2) os_version="oxygenos-10" ;;
+                3) os_version="oxygenos-11" ;;
+                4) show_manufacturer_menu "$cleaner_type"; return ;;
+                5) echo "Exiting..."; exit ;;
+                *) echo "Invalid choice. Please try again."; show_os_version_menu "$manufacturer" "$cleaner_type"; return ;;
+            esac
             ;;
-        5)
-            echo "Exiting..."
-            exit
+        "vivo")
+            case $os_choice in
+                1) os_version="funtouchos-9" ;;
+                2) os_version="funtouchos-10" ;;
+                3) os_version="funtouchos-11" ;;
+                4) show_manufacturer_menu "$cleaner_type"; return ;;
+                5) echo "Exiting..."; exit ;;
+                *) echo "Invalid choice. Please try again."; show_os_version_menu "$manufacturer" "$cleaner_type"; return ;;
+            esac
             ;;
-        *)
-            echo "Invalid choice. Please try again."
-            show_os_version_menu "$manufacturer" "$cleaner_type"
-            return
+        "oppo")
+            case $os_choice in
+                1) os_version="coloros-6" ;;
+                2) os_version="coloros-7" ;;
+                3) os_version="coloros-8" ;;
+                4) show_manufacturer_menu "$cleaner_type"; return ;;
+                5) echo "Exiting..."; exit ;;
+                *) echo "Invalid choice. Please try again."; show_os_version_menu "$manufacturer" "$cleaner_type"; return ;;
+            esac
+            ;;
+        "realme")
+            case $os_choice in
+                1) os_version="realmeui-1" ;;
+                2) os_version="realmeui-2" ;;
+                3) os_version="realmeui-3" ;;
+                4) show_manufacturer_menu "$cleaner_type"; return ;;
+                5) echo "Exiting..."; exit ;;
+                *) echo "Invalid choice. Please try again."; show_os_version_menu "$manufacturer" "$cleaner_type"; return ;;
+            esac
+            ;;
+        "nothing")
+            case $os_choice in
+                1) os_version="nothingos-1" ;;
+                2) os_version="nothingos-2" ;;
+                3) os_version="nothingos-3" ;;
+                4) show_manufacturer_menu "$cleaner_type"; return ;;
+                5) echo "Exiting..."; exit ;;
+                *) echo "Invalid choice. Please try again."; show_os_version_menu "$manufacturer" "$cleaner_type"; return ;;
+            esac
+            ;;
+        "honor")
+            case $os_choice in
+                1) os_version="magicui-2" ;;
+                2) os_version="magicui-3" ;;
+                3) os_version="magicui-4" ;;
+                4) show_manufacturer_menu "$cleaner_type"; return ;;
+                5) echo "Exiting..."; exit ;;
+                *) echo "Invalid choice. Please try again."; show_os_version_menu "$manufacturer" "$cleaner_type"; return ;;
+            esac
+            ;;
+        "motorola")
+            case $os_choice in
+                1) os_version="myux-1" ;;
+                2) os_version="myux-2" ;;
+                3) os_version="myux-3" ;;
+                4) show_manufacturer_menu "$cleaner_type"; return ;;
+                5) echo "Exiting..."; exit ;;
+                *) echo "Invalid choice. Please try again."; show_os_version_menu "$manufacturer" "$cleaner_type"; return ;;
+            esac
+            ;;
+        "meizu")
+            case $os_choice in
+                1) os_version="flymeos-9" ;;
+                2) os_version="flymeos-10" ;;
+                3) os_version="flymeos-11" ;;
+                4) show_manufacturer_menu "$cleaner_type"; return ;;
+                5) echo "Exiting..."; exit ;;
+                *) echo "Invalid choice. Please try again."; show_os_version_menu "$manufacturer" "$cleaner_type"; return ;;
+            esac
             ;;
     esac
 
     remove_bloatware "$manufacturer" "$os_version" "$cleaner_type"
 }
 
-
-
 print_banner
-
 show_cleaner_type_menu
